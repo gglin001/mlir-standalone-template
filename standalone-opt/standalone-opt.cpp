f8f3dbf6f60478433c48e06ebaab5ad8244f403d
Allen Guo <alleng@graphcore.ai>
2022-08-12 10:33:32 +0700

    add standalone-lsp-server

--- standalone-opt/standalone-opt.cpp
+++ standalone-opt/standalone-opt.cpp
@@ -26,13 +26,8 @@ int main(int argc, char **argv) {
   // TODO: Register standalone passes here.
 
   mlir::DialectRegistry registry;
+  registerAllDialects(registry);
   registry.insert<mlir::standalone::StandaloneDialect>();
-  registry.insert<mlir::func::FuncDialect>();
-  registry.insert<mlir::arith::ArithmeticDialect>();
-  // Add the following to include *all* MLIR Core dialects, or selectively
-  // include what you need like above. You only need to register dialects that
-  // will be *parsed* by the tool, not the one generated
-  // registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Standalone optimizer driver\n", registry));
